09a27eee0a94dde634872fd113c524370984ab72
Florian Königsmann <florian.koenigsmann@gmail.com>
2024-11-25 21:34:10 +0100

    Add navigation sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SidebarComponent} from './shared/components/sidebar/sidebar.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent, MatSidenavModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'DBetterWeb';
+  title = 'DBetter';
 }
