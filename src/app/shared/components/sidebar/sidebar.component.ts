2311077a54b75f6754fb76413063d79d5f3609ea
Florian Königsmann <florian.koenigsmann@gmail.com>
2024-11-25 23:04:57 +0100

    Use internationalization package

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,42 +1,20 @@
 import { Component } from '@angular/core';
 import {MatListItem, MatNavList} from '@angular/material/list';
 import {MatIcon} from '@angular/material/icon';
-import {NgForOf} from '@angular/common';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {MatTooltip} from '@angular/material/tooltip';
-export type MenuItem = {
-  icon: string;
-  label: string;
-  route: string;
-}
+
 @Component({
   selector: 'app-sidebar',
   imports: [
     MatNavList,
     MatIcon,
-    NgForOf,
     RouterLink,
     MatTooltip,
     MatListItem,
-    RouterLinkActive
+    RouterLinkActive,
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
-  genericMenu:MenuItem[] = [
-    {
-      icon: 'home',
-      label: 'Home',
-      route: 'home'
-    }
-  ]
-
-  userMenu:MenuItem[] = [
-    {
-      icon: 'person',
-      label: 'Account',
-      route: 'account'
-    }
-  ]
-}
+export class SidebarComponent {}
