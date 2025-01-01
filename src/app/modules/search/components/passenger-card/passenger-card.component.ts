62b50fb73fa6497ec16c20c09d1d14575cdda48d
Florian Königsmann <florian.koenigsmann@gmail.com>
2025-01-01 12:57:02 +0100

    Fix invalid age calculation

--- src/app/modules/search/components/passenger-card/passenger-card.component.ts
+++ src/app/modules/search/components/passenger-card/passenger-card.component.ts
@@ -22,18 +22,22 @@ export class PassengerCardComponent {
   protected readonly getShortTitleOfDiscount = getShortTitleOfDiscount;
 
   public calcPassengerAge(passenger: PassengerDto, tripTime : Date) {
-    if(passenger.age){
+    if(passenger.age != null){
       return passenger.age;
     }
 
     const birthday = new Date(passenger.birthday!);
+    //27.11.2002 00:00
+    //01.01.2025 10:45
+    //2025 - 2002 = 23
+    //
 
     let age = tripTime.getFullYear() - birthday.getFullYear();
     const monthDifference = tripTime.getMonth() - birthday.getMonth();
     const dayDifference = tripTime.getDate() - birthday.getDate();
 
     if(monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)){
-      age++;
+      age--;
     }
 
     return age;
