2c90c93ded4281055f3473468b728d44b4e308a2
Florian Königsmann <florian.koenigsmann@gmail.com>
2025-01-07 23:11:26 +0100

    Make everything deployment ready

--- src/app/shared/interceptors/api.interceptor.ts
+++ src/app/shared/interceptors/api.interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
-const baseUrl = 'https://localhost:7038/';
+const baseUrl = 'https://raspi02.whsfkf8zibahncto.myfritz.net/api/';
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
   req = req.clone({
