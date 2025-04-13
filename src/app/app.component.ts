aaf3ceea813af362fbc0ce8c9c957552e7da3dd7
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2025-04-13 21:52:59 +0200

    Elevate navigation bar to always be visible

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {IconComponent} from './common/icon/icon.component';
 import {NavButtonComponent} from './common/nav-button/nav-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, IconComponent, NavButtonComponent],
+  imports: [RouterOutlet, NavButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
