b1e7bb95b02ea3944264e8eef8c53280569ebf6c
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2025-04-13 17:35:46 +0200

    Implement custom angular material like expansion panel

---
+++ src/app/modules/find-connections/components/expansion-panel/expansion-panel.component.ts
@@ -0,0 +1,34 @@
+import {Component, effect, input, output} from '@angular/core';
+import {IconComponent} from '../../../../common/icon/icon.component';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'expansion-panel',
+  imports: [
+    IconComponent,
+    NgIf
+  ],
+  templateUrl: './expansion-panel.component.html',
+  styleUrl: './expansion-panel.component.scss'
+})
+export class ExpansionPanelComponent {
+  title = input.required<string>();
+  expanded = input<boolean | ''>(false);
+  invalid = input.required<boolean>();
+  invalidChange = output<boolean>();
+
+  expandedChange = output<boolean>();
+
+  isExpanded = false;
+
+  constructor() {
+    effect(() => {
+      this.isExpanded = this.expanded() || this.expanded() === '';
+    });
+  }
+
+  togglePanel(){
+    this.isExpanded = !this.isExpanded;
+    this.expandedChange.emit(this.isExpanded);
+  }
+}
