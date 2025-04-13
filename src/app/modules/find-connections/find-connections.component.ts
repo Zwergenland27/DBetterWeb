97000188c85e84665ed37bc5c1bbeb1cd4a75681
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2025-04-13 21:59:11 +0200

    Implement route options expandable

--- src/app/modules/find-connections/find-connections.component.ts
+++ src/app/modules/find-connections/find-connections.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import {RouteOptionsComponent} from './components/route-options/route-options.component';
+import {ExpansionPanelComponent} from './components/expansion-panel/expansion-panel.component';
+import {TimeOptionsComponent} from './components/time-options/time-options.component';
+import {PassengerOptionsComponent} from './components/passenger-options/passenger-options.component';
+import {FloatingButtonComponent} from '../../common/floating-button/floating-button.component';
+import {IconComponent} from '../../common/icon/icon.component';
 
 @Component({
   selector: 'app-find-connections',
-  imports: [],
+  imports: [
+    RouteOptionsComponent,
+    ExpansionPanelComponent,
+    TimeOptionsComponent,
+    PassengerOptionsComponent,
+    FloatingButtonComponent,
+    IconComponent
+  ],
   templateUrl: './find-connections.component.html',
   styleUrl: './find-connections.component.scss'
 })
 export class FindConnectionsComponent {
+  editMode = true;
 
+  close(){
+    this.editMode = false;
+  }
+
+  open(){
+    this.editMode = true;
+  }
 }
