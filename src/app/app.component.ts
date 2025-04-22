53c5e7bb8e3b92dbd7d050840cd8fed2d753da01
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2025-04-22 23:14:52 +0200

    Refactor and redesign mobile navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavButtonComponent} from './common/nav-button/nav-button.component';
+import {IconComponent} from './common/icon/icon.component';
+import {FloatingButtonComponent} from './common/floating-button/floating-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavButtonComponent],
+  imports: [RouterOutlet, NavButtonComponent, IconComponent, FloatingButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'DBetterWeb';
+  navigationOpened = false;
+
+  openNavigation(){
+    window.scroll({
+      top: 0
+    });
+    this.navigationOpened = true;
+  }
+
+  closeNavigation(){
+    this.navigationOpened = false;
+  }
+
+  protected readonly open = open;
 }
