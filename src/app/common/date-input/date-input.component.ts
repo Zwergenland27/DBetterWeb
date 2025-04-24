74ad3d3b1470346e7f0833c16dc1c208c72658be
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2025-04-24 17:47:37 +0200

    Add date and time picker

---
+++ src/app/common/date-input/date-input.component.ts
@@ -0,0 +1,112 @@
+import {booleanAttribute, Component, effect, input, output} from '@angular/core';
+import {IconButtonMiniComponent} from '../icon-button-mini/icon-button-mini.component';
+import {FormsModule} from '@angular/forms';
+import {ErrorTranslation} from '../error-translation';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'date-input',
+  imports: [
+    IconButtonMiniComponent,
+    FormsModule,
+    NgIf
+  ],
+  templateUrl: './date-input.component.html',
+  styleUrl: './date-input.component.scss'
+})
+export class DateInputComponent {
+  label = input.required<string>();
+  required = input(false, {transform: booleanAttribute});
+  min = input<Date>();
+  max = input<Date>();
+  errorTranslations = input<Record<string, string>>({});
+  errors: ErrorTranslation[] = [];
+  hideErrors = input(false, {transform: booleanAttribute});
+
+  date = input.required<Date>();
+  dateChange = output<{date: Date, valid: boolean}>();
+  _date = '';
+
+  isValid = true;
+
+  inputId = 'input-' + crypto.randomUUID();
+
+  constructor() {
+    effect(() => {
+      this._date = this.date().toISOString().split('T')[0];
+    });
+  }
+
+  toLocalIsoDate(date: Date | undefined){
+    if(date === undefined){
+      return '';
+    }
+
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+  }
+
+  showDialog(input: HTMLInputElement) {
+    input.showPicker();
+  }
+
+  currentDateChange(value: string){
+    this._date = value;
+
+    this.validate();
+    this.dateChange.emit({date: new Date(Date.parse(this._date)), valid: this.isValid});
+  }
+
+  setErrors(errorCodes: string[]){
+    const errors: ErrorTranslation[] = [];
+    const errorTranslations = this.errorTranslations();
+
+    for(let errorCode of errorCodes){
+      const message = errorTranslations[errorCode];
+      if(message){
+        errors.push(ErrorTranslation.Create(errorCode, message));
+      }else{
+        errors.push(ErrorTranslation.CreateWithoutTranslation(errorCode));
+      }
+    }
+
+    this.errors = errors;
+    this.isValid = errors.length <= 0;
+  }
+
+  clearErrors() {
+    this.setErrors([]);
+  }
+
+  onBlur(){
+    this.validate();
+  }
+
+  validate(){
+    if(this.required() && this._date === ''){
+      this.setErrors(["Frontend.Missing"])
+      return;
+    }
+
+    if(this._date != ''){
+      const [year, month, day] = this._date.split('-').map(Number);
+      const date = new Date(year, month - 1, day, 0, 0, 0);
+
+      if(this.min() && date < this.min()!){
+        console.log(date.toISOString(), this.min()!.toISOString());
+        this.setErrors(["Frontend.BeforeMinDate"]);
+        return;
+      }
+
+      if(this.max() && date > this.max()!){
+        this.setErrors(["Frontend.AfterMaxDate"]);
+        return;
+      }
+    }
+
+    this.clearErrors();
+  }
+}
---
+++ src/app/common/time-input/time-input.component.ts
@@ -0,0 +1,86 @@
+import {booleanAttribute, Component, effect, input, output} from '@angular/core';
+import {ErrorTranslation} from '../error-translation';
+import {FormsModule} from '@angular/forms';
+import {IconButtonMiniComponent} from '../icon-button-mini/icon-button-mini.component';
+import {NgIf} from '@angular/common';
+
+@Component({
+  selector: 'time-input',
+  imports: [
+    FormsModule,
+    IconButtonMiniComponent,
+    NgIf
+  ],
+  templateUrl: './time-input.component.html',
+  styleUrl: './time-input.component.scss'
+})
+export class TimeInputComponent {
+  label = input.required<string>();
+  required = input(false, {transform: booleanAttribute});
+  errorTranslations = input<Record<string, string>>({});
+  errors: ErrorTranslation[] = [];
+  hideErrors = input(false, {transform: booleanAttribute});
+
+  time = input<string>('');
+  timeChange = output<{time: string | undefined, valid: boolean}>();
+  _time = '';
+
+  isValid = true;
+
+  inputId = 'input-' + crypto.randomUUID();
+
+  constructor() {
+    effect(() => {
+      this._time = this.time();
+    });
+  }
+
+  showDialog(input: HTMLInputElement) {
+    input.showPicker();
+  }
+
+  currentTimeChange(value: string){
+    this._time = value;
+    this.validate();
+    if(this.isValid){
+      this.timeChange.emit({time: this._time, valid: true});
+    }else{
+      this.timeChange.emit({time: undefined, valid: this.isValid});
+    }
+
+  }
+
+  setErrors(errorCodes: string[]){
+    const errors: ErrorTranslation[] = [];
+    const errorTranslations = this.errorTranslations();
+
+    for(let errorCode of errorCodes){
+      const message = errorTranslations[errorCode];
+      if(message){
+        errors.push(ErrorTranslation.Create(errorCode, message));
+      }else{
+        errors.push(ErrorTranslation.CreateWithoutTranslation(errorCode));
+      }
+    }
+
+    this.errors = errors;
+    this.isValid = errors.length <= 0;
+  }
+
+  clearErrors() {
+    this.setErrors([]);
+  }
+
+  onBlur(){
+    this.validate();
+  }
+
+  validate(){
+    if(this.required() && this._time === ''){
+      this.setErrors(["Frontend.Missing"])
+      return;
+    }
+
+    this.clearErrors();
+  }
+}
