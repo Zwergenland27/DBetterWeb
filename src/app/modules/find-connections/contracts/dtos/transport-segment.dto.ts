46b97691f1d23cea2927172372c546573b9c3419
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2025-05-18 17:33:24 +0200

    Implement new transport segment contract

--- src/app/modules/find-connections/contracts/dtos/transport-segment.dto.ts
+++ src/app/modules/find-connections/contracts/dtos/transport-segment.dto.ts
@@ -1,5 +1,5 @@
-import { DemandDto } from "../../../../common/contracts/dtos/demand.dto";
-import { SegmentDto } from "./segment.dto";
+import { DemandDto } from '../../../../common/contracts/dtos/demand.dto';
+import { SegmentDto } from './segment.dto';
 import {StopDto} from './stop.dto';
 import {BikeCarriageInformationDto} from '../../../../common/contracts/dtos/bike-carriage-information.dto';
 import {CateringInformationDto} from '../../../../common/contracts/dtos/catering-information.dto';
@@ -10,9 +10,35 @@ export interface TransportSegmentDto extends SegmentDto {
   stops: StopDto[];
   operator: string | null;
   destination: string | null;
-  serviceCategory: string;
+  transportCategory: TransportCategory;
   replacementService: true | null;
-  number: string;
+  line: string;
   bikeCarriage: BikeCarriageInformationDto;
   catering: CateringInformationDto;
+}
+
+export enum TransportCategory {
+  HighSpeedTrain = 'HighSpeedTrain',
+  FastTrain = 'FastTrain',
+  RegionalTrain = 'RegionalTrain',
+  SuburbanTrain = 'SuburbanTrain',
+  UndergroundTrain = 'UndergroundTrain',
+  Tram = 'Tram',
+  Bus = 'Bus',
+  Boat = 'Boat',
+  Replacement = 'Replacement',
+}
+
+export function getIconName(transportCategory: TransportCategory): string {
+  switch (transportCategory) {
+    case TransportCategory.HighSpeedTrain: return 'filled_train';
+    case TransportCategory.FastTrain: return 'filled_train';
+    case TransportCategory.RegionalTrain: return 'filled_train';
+    case TransportCategory.SuburbanTrain: return 'metro';
+    case TransportCategory.UndergroundTrain: return 'subway';
+    case TransportCategory.Bus: return 'filled_directions_bus';
+    case TransportCategory.Tram: return 'filled_tram';
+    case TransportCategory.Boat: return 'filled_directions_boat';
+    case TransportCategory.Replacement: return 'filled_bus_railway';
+  }
 }
