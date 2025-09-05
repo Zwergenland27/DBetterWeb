9480f05e9e838916e0ed6574a48c20a0bede84a8
Florian Königsmann <70534154+Zwergenland27@users.noreply.github.com>
2025-09-05 21:07:08 +0200

    Deploy as PWA

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import {provideHttpClient, withFetch, withInterceptors} from '@angular/common/http';
 import {apiInterceptor} from './common/interceptors/api.interceptor';
 import {DatePipe} from '@angular/common';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     DatePipe,
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withFetch(), withInterceptors([apiInterceptor])),]
+    provideHttpClient(withFetch(), withInterceptors([apiInterceptor])), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),]
 };
